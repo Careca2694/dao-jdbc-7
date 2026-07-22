a841ad1d5654798b77629a13f18afb8467a7de24
FRIMINO MANUEL CHIFOIO <fmchifoio@gmail.com>
2026-07-22 06:53:36 -0700

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,6 +28,14 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n\n=== Test 3: Seller findAll ====");
+		
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
