ca563342df5fa2453726b9661489d48f4b8b53c4
FRIMINO MANUEL CHIFOIO <fmchifoio@gmail.com>
2026-07-22 07:36:42 -0700

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -36,6 +37,12 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n\n=== Test 4: Seller insert ====");
+		Seller newSeller = new Seller(null, "Gregorio","gregorio.gmail.com",new Date(),4500.50,department);
+		sellerDao.insert(newSeller);
+		System.out.println("insert new id: " + newSeller.getId());
+		
 
 	}
 
